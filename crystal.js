cad956b489578bcf283bc6b5cb42e6de4f0cb80f
Android-Penguin <49669409+Android-Penguin@users.noreply.github.com>
2021-10-02 02:39:48 +1300

    Colors V1
    - Added first pallet of colors and color shading to crystals.

--- crystal.js
+++ crystal.js
@@ -24,29 +24,42 @@ function randomInt(min, max) { // min and max included
 
 
 //!Draw functions
-function drawTriangle(point1, point2, point3, color) {
+var colorOptions = [
+    {"h":358, "s":100, "l":67},
+    {"h":23, "s":100, "l":65},
+    {"h":44, "s":100, "l":61},
+    {"h":83, "s":68, "l":47},
+    {"h":162, "s":70, "l":59},
+    {"h":222, "s":100, "l":61},
+    {"h":295, "s":100, "l":61},
+    {"h":277, "s":100, "l":68}
+];
+
+function drawTriangle(point1, point2, point3, lineColor, fillColor) {
     c.beginPath();
+    c.lineWidth = 1;
     c.moveTo(point1.x, point1.y);
     c.lineTo(point2.x, point2.y);
     c.lineTo(point3.x, point3.y);
     c.lineTo(point1.x, point1.y);
     c.closePath();
-    c.strokeStyle = color;
+    c.strokeStyle = lineColor;
     c.stroke();
-    c.fillStyle = "white";
+    c.fillStyle = fillColor;
     c.fill();
 }
-function drawQuadrilateral(point1, point2, point3, point4, color) {
+function drawQuadrilateral(point1, point2, point3, point4, lineColor, fillColor) {
     c.beginPath();
+    c.lineWidth = 1;
     c.moveTo(point1.x, point1.y);
     c.lineTo(point2.x, point2.y);
     c.lineTo(point3.x, point3.y);
     c.lineTo(point4.x, point4.y);
     c.lineTo(point1.x, point1.y);
     c.closePath();
-    c.strokeStyle = color;
+    c.strokeStyle = lineColor;
     c.stroke();
-    c.fillStyle = "white";
+    c.fillStyle = fillColor;
     c.fill();
 }
 
@@ -72,6 +85,9 @@ var bottomLeft = {"x":0, "y":0};
 var bottomRight = {"x":0, "y":0};
 var bottomInnerL = {"x":0, "y":0};
 var bottomInnerR = {"x":0, "y":0};
+// Appearance
+var colorIndex;
+var color = {"line":"", "fillLight":"", "fillDark":""};
 // State variables
 var drawState = 0;
 var crystalCount = 0;
@@ -125,16 +141,21 @@ function drawCrystal() {
         c.globalCompositeOperation = "destination-over";
     }
 
+    // Select colour
+    colorIndex = randomInt(0, colorOptions.length-1);
+    color.line = "hsl("+colorOptions[colorIndex].h+","+colorOptions[colorIndex].s+"%,"+colorOptions[colorIndex].l+"%)";
+    color.fillLight = "hsl("+colorOptions[colorIndex].h+","+colorOptions[colorIndex].s+"%,"+(colorOptions[colorIndex].l+20)+"%)";
+    color.fillDark = "hsl("+colorOptions[colorIndex].h+","+(colorOptions[colorIndex].s-10)+"%,"+(colorOptions[colorIndex].l-10)+"%)";
 
     // Draw top of crystal
-    drawTriangle(top, right, innerR, "black");
-    drawTriangle(top, innerR, innerL, "black");
-    drawTriangle(top, innerL, left, "black");
+    drawTriangle(top, right, innerR, color.line, color.fillDark);
+    drawTriangle(top, innerR, innerL, color.line, color.fillLight);
+    drawTriangle(top, innerL, left, color.line, color.fillLight);
 
     // Draw sides of crystal
-    drawQuadrilateral(right, bottomRight, bottomInnerR, innerR, "black");
-    drawQuadrilateral(innerR, bottomInnerR, bottomInnerL, innerL, "black");
-    drawQuadrilateral(left, bottomLeft, bottomInnerL, innerL, "black");
+    drawQuadrilateral(right, bottomRight, bottomInnerR, innerR, color.line, color.fillDark);
+    drawQuadrilateral(innerR, bottomInnerR, bottomInnerL, innerL, color.line, color.fillLight);
+    drawQuadrilateral(left, bottomLeft, bottomInnerL, innerL, color.line, color.fillLight);
 
     crystalCount ++;
 }
